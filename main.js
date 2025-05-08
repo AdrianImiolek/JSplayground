a28358555da1c50b16ebc4ce95e581646091dab1
Adrian Imiolek <adrian.imiolek99@gmail.com>
2025-05-08 08:26:33 +0200

    Added more javascript for adding/removing/buying Items on list

--- main.js
+++ main.js
@@ -1,54 +1,68 @@
 const product = document.querySelector("[data-item]");
 const productAdd = document.querySelector("[data-item-add]");
 const productList = document.querySelector("[data-list]");
-const shoppingBtns = document.querySelector('[data-list-btns');
-
+const shoppingBtns = document.querySelector("[data-list-btns");
+const shoppingBtnRmv = document.querySelectorAll(".shopping-list__btn--remove");
+const shoppingBtnCheck = document.querySelectorAll(".shopping-list__btn--checked");
+const clearBtn = document.querySelector("[data-clear-btn]");
 
 const addProduct = () => {
 	if (product.value === "") {
-		alert("Fill in empty space!");
+		alert("Fill in empty space!"); //If space is empty, alert the user
 	} else {
 		let createLi = document.createElement("li"); //assign a variable that creates an item li
 		createLi.classList.add("shopping-list__listed-item"); //provide an li item with class for styling and proper behavior
-		createLi.textContent = product.value; //provide textContent for an li item that is assigned via value from input
+		createLi.textContent = product.value; //provide textContent for a  item that is assigned via value from input
 		productList.appendChild(createLi); //add a created li item with all it's properties to the list
 
-		let createDiv = document.createElement("div")
-		createDiv.classList.add("shopping-list__buttons");
-		createLi.appendChild(createDiv);
+		// --------------------------------------------------
 
-		let createBtnRmv = document.createElement("button");
-		let createBtnCheck = document.createElement("button");
-		createBtnRmv.classList.add("shopping-list__btn--remove");
-		createBtnCheck.classList.add("shopping-list__btn--checked");
+		let createDiv = document.createElement("div"); //create a div element
+		createDiv.classList.add("shopping-list__buttons"); //provide a class to that element
+		createLi.appendChild(createDiv); //add the element
 
-		createBtnRmv.innerHTML="<i class='bx bx-x'></i>";
-		createBtnCheck.innerHTML="<i class='bx bx-chevron-down'></i>";
+		//----------------------------------------------------
 
-		createDiv.appendChild(createBtnRmv);
-		createDiv.appendChild(createBtnCheck);
+		let createBtnRmv = document.createElement("button"); //create a button element
+		let createBtnCheck = document.createElement("button"); //create a button element
+		createBtnRmv.classList.add("shopping-list__btn--remove", "button"); //provide a class to 1st button
+		createBtnCheck.classList.add("shopping-list__btn--checked", "button"); //provide a class for 2nd button
 
-		product.value = "";
+		createBtnRmv.innerHTML = "<i class='bx bx-x'></i>"; //provide a class for 1st button
+		createBtnCheck.innerHTML = "<i class='bx bx-chevron-down'></i>"; //provide a class for 2nd button
+
+		createDiv.appendChild(createBtnRmv); //append 1st button
+		createDiv.appendChild(createBtnCheck); //append 2nd button
+
+		product.value = ""; //set the initial value from input to empty string;
 	}
 };
 
-function removeProduct() {}
+function clearList() {
+	const allItems = document.querySelectorAll(".shopping-list__listed-item"); // assign a variable with elements containing class .shopping-list__listed-item
+	allItems.forEach(item => item.remove());//loop over every element containting this class and remove them
+}
 
-productAdd.addEventListener("click", addProduct);
+productList.addEventListener("click", (event) => {
+	const buttonRmv = event.target.closest('.shopping-list__btn--remove');
+	if(buttonRmv) {
+		buttonRmv.closest('li').remove();
+	}
+})
+
+productList.addEventListener("click", (event) => {
+	const buttonCheck = event.target.closest(".shopping-list__btn--checked");
+	if(buttonCheck){
+		buttonCheck.closest("li").classList.add("shopping-list__listed-item--bought");
+	}
+})
+
+product.addEventListener("keydown", (event) => {
+	if(event.key === "Enter"){
+		addProduct(); //keydown is best used with inputs
+	}
+})
 
 
-
-let createProduct = document.createElement("li"); //create an element
-
-		// let createBtn = document.createElement("button"); //create a button
-
-		// createProduct.classList.add("shopping-list__listed-item"); //provide a class for created element
-
-		// createProduct.textContent = product.value; //provide textContent based on initial value
-
-		// productList.appendChild(createProduct); //add everything that was made inside the list
-
-		// createBtn.classList.add("shopping-list__btn--remove");
-		// createBtn.classList.add("shopping-list__btn--checked");
-
-		// product.value = ""; //clear item 
+clearBtn.addEventListener("click",clearList);
+productAdd.addEventListener("click",addProduct);
